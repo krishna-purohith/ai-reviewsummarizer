717b259d66c8afc64eaf8e4201f822202130a17d
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-08-20 19:25:34 +0530

    Set up the app connection

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("krishna is creating a new aiproject");
 });
 
+app.get("/api/ai", (req: Request, res: Response) => {
+  res.json({ message: "Server is responding from /api/ai" });
+});
+
 app.listen(port, () =>
   console.log("aiproject app started listening on port: ", port)
 );
