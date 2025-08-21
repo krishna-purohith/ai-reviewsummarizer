de313fdde3ea4915a876a7d82baaf3e2cb80a546
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-08-21 08:32:25 +0530

    Format using prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/ai")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-  return (
-    <div className="p-4">
-      <p className="font-bold text-3xl">{message}</p>
-      <Button>Shadcn</Button>
-    </div>
-  );
+   useEffect(() => {
+      fetch('/api/ai')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button>Shadcn</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,20 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("krishna is creating a new aiproject");
+app.get('/', (req: Request, res: Response) => {
+   res.send('krishna is creating a new aiproject');
 });
 
-app.get("/api/ai", (req: Request, res: Response) => {
-  res.json({ message: "Server is responding from /api/ai" });
+app.get('/api/ai', (req: Request, res: Response) => {
+   res.json({ message: 'Server is responding from /api/ai' });
 });
 
 app.listen(port, () =>
-  console.log("aiproject app started listening on port: ", port)
+   console.log('aiproject app started listening on port: ', port)
 );
