634476c7041e6c7891723e8167feceed83357634
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-08-21 16:01:20 +0530

    Refactor: Extract controller

---
+++ packages/server/controllers/chat.controller.ts
@@ -0,0 +1,35 @@
+import type { Request, Response } from 'express';
+import { chatService } from '../services/chat.service';
+import z from 'zod';
+import { chatRepository } from '../repositories/chat.repository';
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is neccessary.')
+      .max(100, 'Message is to long. Maximum 100 charecters.'),
+   chatId: z.uuid(),
+});
+
+export const chatController = {
+   async sendMessage(req: Request, res: Response) {
+      const validate = chatSchema.safeParse(req.body);
+      if (!validate.success)
+         return res.status(400).json(z.treeifyError(validate.error));
+
+      try {
+         const { prompt, chatId } = req.body;
+         const response = await chatService.sendMessage(prompt, chatId);
+
+         res.json({
+            message: response.message,
+            id: chatRepository.getLastResponseId(chatId),
+         });
+      } catch (error) {
+         res.status(500).json({
+            error: 'Internal server Error, Try after sometime.',
+         });
+      }
+   },
+};
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,7 @@
-import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import z from 'zod';
-import { chatRepository } from './repositories/chat.repository';
-import { chatService } from './services/chat.service';
+import type { Request, Response } from 'express';
+import express from 'express';
+import { chatController } from './controllers/chat.controller';
 
 dotenv.config();
 
@@ -11,34 +9,7 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const chatSchema = z.object({
-   prompt: z
-      .string()
-      .trim()
-      .min(1, 'Prompt is neccessary.')
-      .max(100, 'Message is to long. Maximum 100 charecters.'),
-   chatId: z.uuid(),
-});
-
-app.post('/api/c', async (req: Request, res: Response) => {
-   const validate = chatSchema.safeParse(req.body);
-   if (!validate.success)
-      return res.status(400).json(z.treeifyError(validate.error));
-
-   try {
-      const { prompt, chatId } = req.body;
-      const response = await chatService.sendMessage(prompt, chatId);
-
-      res.json({
-         message: response.message,
-         id: chatRepository.getLastResponseId(chatId),
-      });
-   } catch (error) {
-      res.status(500).json({
-         error: 'Internal server Error, Try after sometime.',
-      });
-   }
-});
+app.post('/api/c', chatController.sendMessage);
 
 app.get('/', (req: Request, res: Response) => {
    res.send('krishna is creating a new aiproject');
