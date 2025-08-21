694621da7a21b8bdae7c48ca3ce4205810e438d2
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-08-21 15:08:07 +0530

    Build the backend first route

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,57 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
+
+const chats = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is neccessary.')
+      .max(100, 'Message is to long. Maximum 100 charecters.'),
+   chatId: z.uuid(),
+});
+
+app.post('/api/c', async (req: Request, res: Response) => {
+   const validate = chatSchema.safeParse(req.body);
+   if (!validate.success)
+      return res.status(400).json(z.treeifyError(validate.error));
+
+   try {
+      const { prompt, chatId } = req.body;
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini!',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 30,
+         previous_response_id: chats.get(chatId),
+      });
+
+      chats.set(chatId, response.id);
+
+      res.json({
+         message: response.output_text,
+         id: chats.get(chatId),
+      });
+   } catch (error) {
+      res.status(500).json({
+         error: 'Internal server Error, Try after sometime.',
+      });
+   }
+});
 
 app.get('/', (req: Request, res: Response) => {
    res.send('krishna is creating a new aiproject');
