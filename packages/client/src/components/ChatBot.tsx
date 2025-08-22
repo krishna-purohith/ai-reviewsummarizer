469d886737039df3fdf32ad015bb908f03481715
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-08-22 10:54:43 +0530

    Fix the copied text

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -49,12 +49,20 @@ const ChatBot = () => {
       }
    };
 
+   const onCopyHandler = (e: React.ClipboardEvent) => {
+      const selection = window.getSelection()?.toString().trim();
+      if (selection) {
+         e.preventDefault();
+         e.clipboardData.setData('text/plain', selection);
+      }
+   };
    return (
       <div>
          <div className="flex flex-col gap-4 mb-10">
             {messages.map((message, index) => (
                <p
                   key={index}
+                  onCopy={onCopyHandler}
                   className={`
                      px-3 py-1 rounded-4xl
                      ${message.role === 'user' ? 'bg-indigo-500 text-white self-end' : 'bg-slate-200 text-slate-900 self-start'}`}
